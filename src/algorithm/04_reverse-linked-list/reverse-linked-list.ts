4dee624a64b44fbe875f5193ad0e8d00db4b584b
zhuotuo <2301140231@qq.com>
2023-08-09 21:58:28 +0800

    04-反转单向链表

--- src/algorithm/04_reverse-linked-list/reverse-linked-list.ts
+++ src/algorithm/04_reverse-linked-list/reverse-linked-list.ts
@@ -2,16 +2,44 @@
  * @description 反转单向链表
  * @author shuibuzhuo
  */
-type LinkedListNode = {
+export type LinkedListNode = {
   value: number,
   next?: LinkedListNode
 }
+
+export function reverseLinkedList(head: LinkedListNode): LinkedListNode {
+  let prevNode: LinkedListNode | undefined = undefined
+  let curNode: LinkedListNode | undefined = undefined
+  let nextNode: LinkedListNode | undefined = head
+
+  while(nextNode) {
+    // 处理第一个元素，删除 next，避免循环引用
+    if (curNode && !prevNode) {
+      delete curNode.next
+    }
+
+    // 反转指针
+    if (curNode && prevNode) {
+      curNode.next = prevNode
+    }
+    
+    // 往后移动 3 个指针
+    prevNode = curNode
+    curNode = nextNode
+    nextNode = nextNode.next
+  }
+
+  // 处理最后一个元素
+  curNode!.next = prevNode
+
+  return curNode!
+} 
 
 /**
  * 根据数组，创建单向链表
  * @param arr 输入的数组
  */
-function createLinkedList(arr: number[]): LinkedListNode {
+export function createLinkedList(arr: number[]): LinkedListNode {
   const length = arr.length
   if (length === 0) throw new Error('arr is empty')
 
@@ -33,5 +61,6 @@ function createLinkedList(arr: number[]): LinkedListNode {
 
 // 功能测试
 const arr = [100, 200, 300, 400, 500]
-const res = createLinkedList(arr)
-console.log('res', res)
+const node = createLinkedList(arr)
+const node1 = reverseLinkedList(node)
+console.log('node1', node1)
