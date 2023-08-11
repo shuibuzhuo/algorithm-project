acd9b7e09f6fe2bba9053e349a341549fd05970b
shuibuzhuo <2301140231@qq.com>
2023-08-11 12:01:43 +0800

    06-两数之和，双指针方法

--- src/algorithm/06_two-numbers-sum/two-numbers-sum.test.ts
+++ src/algorithm/06_two-numbers-sum/two-numbers-sum.test.ts
@@ -4,26 +4,26 @@
  */
 
 import { describe, expect, it } from "vitest";
-import { findTwoNumbers } from "./two-numbers-sum";
+import { findTwoNumbers1, findTwoNumbers2 } from "./two-numbers-sum";
 
 describe('两数之和', () => {
   it('正常情况', () => {
     const arr = [10, 30, 50, 70, 80]
     const n = 100
-    const res = findTwoNumbers(arr, n)
+    const res = findTwoNumbers2(arr, n)
     expect(res).toEqual([30, 70])
   })
 
   it('空数组', () => {
     const n = 100
-    const res = findTwoNumbers([], n)
+    const res = findTwoNumbers2([], n)
     expect(res).toEqual([])
   })
 
   it('找不到和等于 n 的两个数', () => {
     const arr = [10, 30, 50, 70, 90]
     const n = 700
-    const res = findTwoNumbers(arr, n)
+    const res = findTwoNumbers2(arr, n)
     expect(res).toEqual([])
   })
 })
--- src/algorithm/06_two-numbers-sum/two-numbers-sum.ts
+++ src/algorithm/06_two-numbers-sum/two-numbers-sum.ts
@@ -4,40 +4,97 @@
  */
 
 /**
- * @description 寻找和为 n 的两个数
+ * @description 寻找和为 n 的两个数（嵌套循环）
  * @param arr 输入的数组
  * @param n 和
  * @returns 找出来的两个数
  */
-export function findTwoNumbers(arr: number[], n: number): number[] {
+export function findTwoNumbers1(arr: number[], n: number): number[] {
   const res: number[] = []
   
   const length = arr.length
   if (length === 0) return res
 
   for (let i = 0; i < length; i++) {
+    // 先取出一个数
     const n1 = arr[i]
+
+    // 定义一个标记，标记是否已经找到了另一个数
     let flag = false
 
     for (let j = i + 1; j < length; j++) {
+      // 从第一个数的后一位开始找第二个数，即 i + 1
       const n2 = arr[j]
+
       if (n1 + n2 === n) {
+        // 如果找到了
         flag = true
         res.push(n1)
         res.push(n2)
+        // 提前终止循环，避免不必要的循环
         break
       }
     }
 
+    // 如果找到了那两个数，提前终止循环，避免不必要的循环
     if (flag) break
   }
 
   return res
 }
 
-// 功能测试
-const arr = [10, 30, 50, 70, 80]
-const res = findTwoNumbers(arr, 100)
-console.log('res', res)
+/**
+ * @description 寻找和为 n 的两个数（双指针）
+ * @param arr 输入的数组
+ * @param n 和
+ * @returns 找出来的两个数
+ */
+export function findTwoNumbers2(arr: number[], n: number): number[] {
+  const res: number[] = []
 
+  const length = arr.length
+  if (length === 0) return res
+
+  // 利用双指针
+  let i = 0
+  let j = length - 1
+
+  // 两个指针还没相遇的时候，进行循环
+  while(i < j) {
+    const n1 = arr[i]
+    const n2 = arr[j]
+    const sum = n1 + n2
+    if (sum < n) {
+      // 如果两数的和较小，则 i 向后移动
+      i++
+    } else if (sum > n) {
+      // 如果两数的和较大，则 j 向前移动
+      j--
+    } else {
+      res.push(n1)
+      res.push(n2)
+      break;
+    }
+  }
+
+  return res
+}
+
+// 功能测试
+const arr = [5, 10, 5, 10,5, 10,5, 10,5, 10,5, 10,5, 10,5, 10,5, 10,5, 10,5, 10, 30, 50, 70, 80]
+// const res = findTwoNumbers2(arr, 100)
+// console.log('res', res)
+
+// 性能测试
+console.time('嵌套循环')
+for (let i = 0; i < 100 * 10000; i++) {
+  findTwoNumbers1(arr, 100)
+}
+console.timeEnd('嵌套循环') // 399ms
+
+console.time('双指针')
+for (let i = 0; i < 100 * 10000; i++) {
+  findTwoNumbers2(arr, 100)
+}
+console.timeEnd('双指针') // 49ms
 export {}
